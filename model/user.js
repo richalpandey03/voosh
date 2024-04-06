82d255bcce1d41c778b75ff44d48eb7e2782f148
richalpandey03 <66243723+richalpandey03@users.noreply.github.com>
2024-04-06 14:53:50 +0530

    User model

---
+++ model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email :{
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role : {
+    type: String,
+    required: true
+  },
+  profile_type :{
+    type : String,
+    required : true
+  }
+});
+
+module.exports = mongoose.model('users', userSchema);
